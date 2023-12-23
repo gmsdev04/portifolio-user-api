afcf0c9cf5ec0d66ad164eb366e48c6ece1084aa
gmsdev04 <gusiqueira@uolinc.com>
2023-12-22 23:34:14 -0300

    including project

---
+++ src/main/java/br/com/mercadolivre/meliuserapi/MeliUserApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.meliuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliUserApiApplication.class, args);
+	}
+
+}
